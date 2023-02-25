5f49cf55f7a1aebd26ecb6a35230675c7771e254
Timon Schlenk <timon.schlenk@gmx.de>
2023-02-25 16:19:19 +0100

    disabled css for Homepage, working progress und Design of the Question page

--- src/pages/QuestionPage/QuestionPage.tsx
+++ src/pages/QuestionPage/QuestionPage.tsx
@@ -7,10 +7,16 @@ interface IQuestionSiteProps {
 
 const QuestionPage:React.FC<IQuestionSiteProps> = (Props:IQuestionSiteProps):JSX.Element => {
 
-    return <div>
+    return <div id="page">
             <h1>Der Wahl-O-Mat</h1>
             <h2>Frage 1 von 40</h2>
-            <div id="Questions"></div>
+            <div id="Questions">
+                <h3>Schwörst du Bruder aufs kleine schwarze Büchlein, dass du kämpfen wirst um Einigkeit und Recht und Fortschritt herbeizuführen?</h3>
+                <div id="Answers">
+                    <button id="Yes">Ja</button>
+                    <button id="No">Nein</button>
+                </div>
+            </div>
         </div>
 }
 
