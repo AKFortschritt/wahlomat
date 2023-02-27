862d17b39e4372afdb95413c7dc48d4f3f544543
Niklas Gabriel <nikgab@gmx.de>
2023-02-27 08:49:54 +0100

    CSS changes

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -10,26 +10,23 @@ const HomePage:React.FC<IHomePageProps> = (Props:IHomePageProps):JSX.Element => 
 
     const navigate = useNavigate();
 
-    return <div id = "page">
-                <div id="header">
-                    <h1 id="header1">Der Wahl-O-Mat</h1>
-                    <h2 id="header2">Ein Projekt des AK Fortschritt</h2>
-                </div>
-                <div id="content">
-                    <div id="main">
-                        <p id="explanation">Der Wahl-O-Mat ist eine Tool erstellt von Schülern mit Verantwortung für Schüler mit Verantwortung. Er soll dazu dienen für einen passende, an der Schule angebotene Wahlkurse und AKs aber auch Wettbewerbe zu finden und kann auch zur Wahl von P und W Seminar sowie weiteren Abiturfächern benutzt werden. Mit dem Wahl-O-Maten könnt ihr von der Erfahrung vorheriger Schüler und der Einschätzung vieler Profitieren um das Richtige für euch zu finden</p>
-                        <div id="start-container">
-                            <button id="start" className="animate float animate--infinite animate--slow" 
-                                onClick = {() =>  navigate("/questions")}>Start</button>
+    return <div id="HPage">
+                <div id="HContent">
+                    <h1>Der Wahl-O-Mat</h1>
+                    <h2>Ein Projekt des AK Fortschritt</h2>
+                    <div id="HMain">
+                        <p>Der Wahl-O-Mat ist eine Tool erstellt von Schülern mit Verantwortung für Schüler mit Verantwortung. Er soll dazu dienen für einen passende, an der Schule angebotene Wahlkurse und AKs aber auch Wettbewerbe zu finden und kann auch zur Wahl von P und W Seminar sowie weiteren Abiturfächern benutzt werden. Mit dem Wahl-O-Maten könnt ihr von der Erfahrung vorheriger Schüler und der Einschätzung vieler Profitieren um das Richtige für euch zu finden</p>
+                        <div id="HStartContainer">
+                            <button id="HStartBtn" className="animate" onClick = {() =>  navigate("/questions")}>Start</button>
                         </div>
                     </div>
-                    <div id="thanks-container">
-                        <p id="thanks">Der Wahl-O-Mat wurde erstellt vom <a href="blank">AK-Fortschritt</a>, viel Spaß ! </p>
+                    <div id="HTanksContainer">
+                        <p>Der Wahl-O-Mat wurde erstellt vom <a href="blank">AK-Fortschritt</a>, viel Spaß!</p>
                         <p>Wir kümmern uns um die kleinen Dinge im Leben!</p>
-                        <div id="fortschritt"></div>
+                        <div id="HFortschritt"></div>
                     </div>
                 </div>
-            </div>
+           </div>
 }
 
 export default HomePage;
--- src/pages/QuestionPage/QuestionPage.tsx
+++ src/pages/QuestionPage/QuestionPage.tsx
@@ -7,17 +7,19 @@ interface IQuestionSiteProps {
 
 const QuestionPage:React.FC<IQuestionSiteProps> = (Props:IQuestionSiteProps):JSX.Element => {
 
-    return <div id="page">
-            <h1>Der Wahl-O-Mat</h1>
-            <h2>Frage 1 von 40</h2>
-            <div id="questions">
-                <p id="question_paragraph">Schwörst du Bruder aufs kleine schwarze Büchlein, dass du kämpfen wirst um Einigkeit und Recht und Fortschritt herbeizuführen?</p>
-                <div id="question_answers">
-                    <button id="yes">Ja</button>
-                    <button id="no">Nein</button>
+    return <div id="QPage">
+                <div id="QContent">
+                    <h1>Der Wahl-O-Mat</h1>
+                    <h2>Frage 1 von 40</h2>
+                    <div id="QMain">
+                        <p id="QParagraph">Schwörst du Bruder aufs kleine schwarze Büchlein, dass du kämpfen wirst um Einigkeit und Recht und Fortschritt herbeizuführen?</p>
+                        <div id="QAnswers">
+                            <button id="QYes">Ja</button>
+                            <button id="QNo">Nein</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+           </div>
 }
 
 export default QuestionPage;
